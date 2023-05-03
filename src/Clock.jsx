d18fa7781f978b4700b7d36be647007e1e79a491
hayashijp296 <lamnq29@gmail.com>
2023-05-03 21:22:39 +0700

    Todo jobs

---
+++ src/Clock.jsx
@@ -0,0 +1,27 @@
+// import React from "react";
+
+// export default class Clock extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             time: new Date().toLocaleTimeString(),
+//         };
+//         this.date = "1/5/2023";
+//     }
+    // getTime = () => {
+    //     this.setState({
+    //         time: new Date().toLocaleTimeString(),
+    //     });
+    // };
+
+    // render() {
+    //     return (
+    //         <div>
+    //             <h1>Hello, world!</h1>
+    //             <h2>It is {this.state.time}</h2>
+    //             <h3>It is {this.date}</h3>
+    //             {/* <button onClick={this.getTime()}></button> */}
+    //         </div>
+    //     );
+    }
+// }
--- src/header.jsx
+++ src/header.jsx
@@ -1,1 +1,19 @@
-const element = <h1>hello, world!</h1>;
+import React from "react";
+
+// class Welcome extends React.Component {
+//     render() {
+//         console.log(this.props);
+//         return <h1>Hello, {this.props.name}</h1>;
+//     }
+// }
+
+function Welcome(props) {
+    console.log(props);
+    return (
+        <h1>
+            Hello, {props.name} - {props.age}
+        </h1>
+    );
+}
+
+export default Welcome;
